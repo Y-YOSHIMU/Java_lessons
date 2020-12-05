e372db13cd73c221fd568f72dfddf35d04ded057
vagrant <vagrant@localhost.localdomain>
2020-12-05 13:41:58 +0000

    [Fin]#05

--- MyApp.java
+++ MyApp.java
@@ -1,9 +1,20 @@
 public class MyApp {
 
     public static void main(String[] args) {
-        // String msg;
-        // msg = "Hello World Again!";
-        String msg = "Hello World Again!";
+        // 文字
+        char a = 'a'; // シングルクォーテーション
+        // 整数 byte short int long
+        int x = 10;
+        long y = 555555555L; // 最後にLをつける
+        // 浮動小数点数 float double
+        double d = 23423.344;
+        float f = 32.33F; // 最後にFをつける
+        // 論理値
+        boolean flag = true; // or false
+        // 文字列
+        // \n 改行
+        // \t タブ
+        String msg = "Hello Wo\nrld Again\t!"; // ダブルクォーテーション
 
         System.out.println(msg);
     }
