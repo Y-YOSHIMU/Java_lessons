00d0a37a9f2b9b5a42025a1bbcae5500db92e0c5
vagrant <vagrant@localhost.localdomain>
2020-12-05 14:52:38 +0000

    [Fin]#08

--- MyApp.java
+++ MyApp.java
@@ -1,13 +1,19 @@
 public class MyApp {
 
     public static void main(String[] args) {
-        // キャスト (他のデータ型に変換すること)
-        // double d = 52343.231;
-        // int i = (int)d;
-
-        int i = 10;
-        double d = (double)i / 4;
-        System.out.println(d);
+        // if
+        // > >= < <= == !=
+        // && || !
+        int score = 78;
+        // if (score > 80) {
+        //     System.out.println("Great!");
+        // } else if (score > 60) {
+        //     System.out.println("Good!");
+        // } else {
+        //     System.out.println("so so ... !");
+        // }
+        String msg = score > 80 ? "Great" : "so so ... !";
+        System.out.println(msg);
     }
 
 }
