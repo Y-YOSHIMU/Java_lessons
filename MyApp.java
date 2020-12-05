158e0a8c9d5603f5ed0de88fd0bea34e9b28b97a
vagrant <vagrant@localhost.localdomain>
2020-12-05 13:48:43 +0000

    [Fin]#06

--- MyApp.java
+++ MyApp.java
@@ -1,22 +1,28 @@
 public class MyApp {
 
     public static void main(String[] args) {
-        // 文字
-        char a = 'a'; // シングルクォーテーション
-        // 整数 byte short int long
-        int x = 10;
-        long y = 555555555L; // 最後にLをつける
-        // 浮動小数点数 float double
-        double d = 23423.344;
-        float f = 32.33F; // 最後にFをつける
-        // 論理値
-        boolean flag = true; // or false
-        // 文字列
-        // \n 改行
-        // \t タブ
-        String msg = "Hello Wo\nrld Again\t!"; // ダブルクォーテーション
+        // 演算
+        // + - * / %
+        // ++ --
+        // int i;
+        // i = 10 / 3;
+        // System.out.println(i); // 3
+        // i = 10 % 3;
+        // System.out.println(i); // 1
+        // int x = 5;
+        // x++;
+        // System.out.println(x); // 6
+        // x--;
+        // System.out.println(x); // 5
 
-        System.out.println(msg);
+        // int x = 5;
+        // // x = x + 12;
+        // x += 12;
+        // System.out.println(x); // 17
+
+        String s;
+        s = "hello " + "world";
+        System.out.println(s);
     }
 
 }
