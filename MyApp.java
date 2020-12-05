643921cd238c1b13a13d0d404587a1d7df6a877f
vagrant <vagrant@localhost.localdomain>
2020-12-05 14:45:21 +0000

    [Fin]#07

--- MyApp.java
+++ MyApp.java
@@ -1,28 +1,13 @@
 public class MyApp {
 
     public static void main(String[] args) {
-        // 演算
-        // + - * / %
-        // ++ --
-        // int i;
-        // i = 10 / 3;
-        // System.out.println(i); // 3
-        // i = 10 % 3;
-        // System.out.println(i); // 1
-        // int x = 5;
-        // x++;
-        // System.out.println(x); // 6
-        // x--;
-        // System.out.println(x); // 5
+        // キャスト (他のデータ型に変換すること)
+        // double d = 52343.231;
+        // int i = (int)d;
 
-        // int x = 5;
-        // // x = x + 12;
-        // x += 12;
-        // System.out.println(x); // 17
-
-        String s;
-        s = "hello " + "world";
-        System.out.println(s);
+        int i = 10;
+        double d = (double)i / 4;
+        System.out.println(d);
     }
 
 }
