d602dd440ec395eb4998ed138c4a9adbc0ae80f1
vagrant <vagrant@localhost.localdomain>
2020-12-06 13:53:12 +0000

    [Fin]#11

--- MyApp.java
+++ MyApp.java
@@ -1,16 +1,14 @@
 public class MyApp {
 
     public static void main(String[] args) {
-        // while
+        // for
         // 0 - 9
-        int i = 100;
-        // while (i < 10) {
-        //     System.out.println(i);
-        //     i++;
-        // }
-        do {
+        for (int i = 0; i < 10; i++) {
+            if (i == 5) {
+                // break;
+                continue;
+            }
             System.out.println(i);
-            i++;
-        } while (i < 10);
+        }
     }
 }
