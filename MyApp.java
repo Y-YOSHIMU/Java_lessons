b121a3bbd409aace128bb2b2fa9c86aa6115f153
vagrant <vagrant@localhost.localdomain>
2020-12-07 13:55:05 +0000

    [Fin]#16

--- MyApp.java
+++ MyApp.java
@@ -1,23 +1,19 @@
 public class MyApp {
 
+    // method
+
+    // public static void sayHi(String name) {
+    public static String sayHi(String name) {
+        // System.out.println("Hi!");
+        // System.out.println("Hi! " + name);
+        return "Hi! " + name;
+    }
+
     public static void main(String[] args) {
-        // int x = 10;
-        // int y = x;
-        // y = 5;
-        //     System.out.println(x);
-        //     System.out.println(y);
-
-        // int[] a = {3, 5, 7};
-        // int[] b = a; // 同じデータを参照している
-        // b[1] = 8; // よってa[1]と同じデータなのでaの値も変わる
-        // System.out.println(a[1]);
-        // System.out.println(b[1]);
-
-        String s = "hello";
-        String t = s;
-        t = "world";
-        System.out.println(s);
-        System.out.println(t);
-
+        // sayHi();
+        // sayHi("Tom");
+        // sayHi("Bob");
+        String msg = sayHi("Steve");
+        System.out.println(msg);
     }
 }
