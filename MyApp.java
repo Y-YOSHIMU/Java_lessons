a3113bb64cdda7ccda678702d041a59f947a62ad
vagrant <vagrant@localhost.localdomain>
2020-12-07 14:00:49 +0000

    [Fin]#17

--- MyApp.java
+++ MyApp.java
@@ -1,19 +1,20 @@
 public class MyApp {
 
-    // method
+    public static void sayHi(String name) {
+        // int x = 10;
+        System.out.println("Hi! " + name);
+    }
 
-    // public static void sayHi(String name) {
-    public static String sayHi(String name) {
-        // System.out.println("Hi!");
-        // System.out.println("Hi! " + name);
-        return "Hi! " + name;
+    // overload
+
+    public static void sayHi() {
+        System.out.println("Hi! Nobody!");
     }
 
     public static void main(String[] args) {
-        // sayHi();
-        // sayHi("Tom");
-        // sayHi("Bob");
-        String msg = sayHi("Steve");
-        System.out.println(msg);
+        sayHi();
+        sayHi("Steve"); // 引数
+        // System.out.println(name);
+        // System.out.println(x);
     }
 }
