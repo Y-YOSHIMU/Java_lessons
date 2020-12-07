8eb5face893fca37a67c39a67c11dbb70de0aef0
vagrant <vagrant@localhost.localdomain>
2020-12-07 14:07:01 +0000

    [Fin]#18

--- MyApp.java
+++ MyApp.java
@@ -1,20 +1,18 @@
+class User {
+    String name = "Me!"; // フィールド classに属する変数
+
+    void sayHi() {
+        System.out.println("hi!");
+    }
+}
+
 public class MyApp {
 
-    public static void sayHi(String name) {
-        // int x = 10;
-        System.out.println("Hi! " + name);
-    }
-
-    // overload
-
-    public static void sayHi() {
-        System.out.println("Hi! Nobody!");
-    }
-
     public static void main(String[] args) {
-        sayHi();
-        sayHi("Steve"); // 引数
-        // System.out.println(name);
-        // System.out.println(x);
+        // Class
+        User tom;
+        tom = new User(); // インスタンス
+        System.out.println(tom.name);
+        tom.sayHi();
     }
 }
