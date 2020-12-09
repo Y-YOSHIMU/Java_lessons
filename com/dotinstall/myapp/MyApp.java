232c46d1aec8e47956790a5e9c27c23303485504
vagrant <vagrant@localhost.localdomain>
2020-12-09 08:41:05 +0000

    [Fin]#29

--- com/dotinstall/myapp/MyApp.java
+++ com/dotinstall/myapp/MyApp.java
@@ -4,12 +4,12 @@ import com.dotinstall.myapp.model.AdminUser;
 import com.dotinstall.myapp.model.AmericanUser;
 import com.dotinstall.myapp.model.JapaneseUser;
 
+
 public class MyApp {
 
     public static void main(String[] args) {
-        AmericanUser tom = new AmericanUser();
-        JapaneseUser aki = new JapaneseUser();
-        tom.sayHi();
-        aki.sayHi();
+        User tom = new User();
+        tom.print();
+        tom.getInfo();
     }
 }
--- com/dotinstall/myapp/model/User.java
+++ com/dotinstall/myapp/model/User.java
@@ -2,6 +2,9 @@ package com.dotinstall.myapp.model;
 
 //抽象クラス -> 具象クラス
 
-public abstract class User {
-    public abstract void sayHi(); // 抽象メソッド
+public class User implements Printable {
+    @Override
+    public void print() {
+        System.out.println("Now printing user profile...");
+    }
 }
