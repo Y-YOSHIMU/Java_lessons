be50e85b13beeaa0d6979328cabcb98e2290a58d
vagrant <vagrant@localhost.localdomain>
2020-12-10 12:10:48 +0000

    [Fin]#31

--- com/dotinstall/myapp/MyApp.java
+++ com/dotinstall/myapp/MyApp.java
@@ -4,29 +4,33 @@ import com.dotinstall.myapp.model.AdminUser;
 import com.dotinstall.myapp.model.AmericanUser;
 import com.dotinstall.myapp.model.JapaneseUser;
 
-// 列挙型
+// 例外
 
-enum Result {
-    SUCCESS, // 0
-    ERROR, // 1
+class MyException extends Exception {
+    public MyException(String s) {
+        super(s);
+    }
 }
 
 public class MyApp {
 
-    public static void main(String[] args) {
-        Result res;
-
-        res = Result.SUCCESS;
-
-        switch (res) {
-            case SUCCESS:
-                System.out.println("OK!");
-                System.out.println(res.ordinal()); // 0
-                break;
-            case ERROR:
-                System.out.println("NG!");
-                System.out.println(res.ordinal()); // 1
-                break;
+    public static void div(int a, int b) {
+        try {
+            if (b < 0) {
+                throw new MyException("not minus!");
+            }
+            System.out.println(a / b);
+        } catch (ArithmeticException e) {
+            System.err.println(e.getMessage());
+        } catch (MyException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            System.out.println(" -- end -- ");
         }
+    }
+
+    public static void main(String[] args) {
+        div(3, 0);
+        div(5, -2);
     }
 }
