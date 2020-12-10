73bd54808a850d23b06da62532e354908f6f7bc9
vagrant <vagrant@localhost.localdomain>
2020-12-10 13:46:53 +0000

    [Fin]#35

--- com/dotinstall/myapp/MyApp.java
+++ com/dotinstall/myapp/MyApp.java
@@ -6,21 +6,41 @@ import com.dotinstall.myapp.model.JapaneseUser;
 
 // Thread
 
-class MyRunnable implements Runnable {
-    @Override
-    public void run() {
-        for (int i = 0; i < 100; i++) {
-            System.out.print('*');
-        }
-    }
-}
+// class MyRunnable implements Runnable { // 関数型インターフェース
+//     @Override
+//     public void run() {
+//         for (int i = 0; i < 100; i++) {
+//             System.out.print('*');
+//         }
+//     }
+// }
 
 public class MyApp {
 
     public static void main(String[] args) {
-        MyRunnable r = new MyRunnable();
-        Thread t = new Thread(r);
-        t.start();
+        // MyRunnable r = new MyRunnable();
+        // Thread t = new Thread(r);
+        // t.start();
+
+        // new Thread(new MyRunnable()).start(); // 無名クラス
+        
+        // new Thread(new Runnable() {
+        //     @Override
+        //     public void run() {
+        //         for (int i = 0; i < 100; i++) {
+        //             System.out.print('*');
+        //         }
+        //     }
+        // }).start();
+
+        // ラムダ式
+        // (引数) -> {処理}        
+
+        new Thread(() -> {
+            for (int i = 0; i < 500; i++) {
+                System.out.print('*');
+            }
+        }).start();
 
         for (int i = 0; i < 500; i++) {
             System.out.print('.');
