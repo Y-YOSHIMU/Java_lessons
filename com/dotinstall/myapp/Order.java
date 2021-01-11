0edd7365a5382c26f563f0e94504a5a8debdd45b
Y-YOSHIMU <mcu7.oldtr.uc@gmail.com>
2021-01-11 22:42:39 +0900

    [Add]If-Then-Else-If

--- com/dotinstall/myapp/Order.java
+++ com/dotinstall/myapp/Order.java
@@ -1,21 +1,41 @@
 public class Order {
+    boolean isFilled;
+    double billAmount;
+    String shipping;
 
-    public static void main(String[] args) {
-
-        double itemCost = 30.99;
-
-        // Write an if-then statement:
-        if (itemCost > 24.00) {
+    public Order(boolean filled, double cost, String shippingMethod) {
+        if (cost > 24.00) {
             System.out.println("High value item!");
         }
+        isFilled = filled;
+        billAmount = cost;
+        shipping = shippingMethod;
+    }
 
-        boolean isFilled = false;
-
-        // Write an if-then-else statement:
+    public void ship() {
         if (isFilled) {
             System.out.println("Shipping");
+            System.out.println("Shipping cost: " + calculateShipping());
         } else {
             System.out.println("Order not ready");
         }
+    }
+
+    public double calculateShipping() {
+        if (shipping == "Regular") {
+            return 0;
+        } else if (shipping == "Express") {
+            return 1.75;
+        } else {
+            return .50;
+        }
+    }
+
+    public static void main(String[] args) {
+        Order book = new Order(true, 9.99, "Express");
+        Order chemistrySet = new Order(false, 72.50, "Regular");
+
+        book.ship();
+        chemistrySet.ship();
     }
 }
