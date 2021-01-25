f3c396d9303848b393d16aa12e31dbd69586ff3d
Y-YOSHIMU <mcu7.oldtr.uc@gmail.com>
2021-01-25 22:43:21 +0900

    [Add]Adding an Item

--- com/dotinstall/myapp/ToDos.java
+++ com/dotinstall/myapp/ToDos.java
@@ -4,5 +4,14 @@ class ToDos {
     
     public static void main(String[] args) {
         ArrayList<String> toDoList = new ArrayList<String>();
+        String toDo1 = "Water plants";
+        String toDo2 = "Study Java";
+        String toDo3 = "Wash dishes";
+
+        toDoList.add(toDo1);
+        toDoList.add(toDo2);
+        toDoList.add(toDo3);
+
+        System.out.println(toDoList);
     }
 }
