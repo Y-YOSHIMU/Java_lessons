1d4c4fc2f78dd0a31613202c47cd2b7a2be118c4
Y-YOSHIMU <mcu7.oldtr.uc@gmail.com>
2021-01-26 22:14:19 +0900

    [Add]ArrayList Size

--- com/dotinstall/myapp/ToDos.java
+++ com/dotinstall/myapp/ToDos.java
@@ -13,5 +13,27 @@ class ToDos {
         toDoList.add(toDo3);
 
         System.out.println(toDoList);
+
+        ArrayList<String> sherlocksToDos = new ArrayList<String>();
+
+        sherlocksToDos.add("visit the crime scene");
+        sherlocksToDos.add("play violin");
+        sherlocksToDos.add("interview suspects");
+        sherlocksToDos.add("solve the case");
+        sherlocksToDos.add("apprehend the criminal");
+
+        ArrayList<String> poirotsToDos = new ArrayList<String>();
+
+        poirotsToDos.add("visit the crime scene");
+        poirotsToDos.add("interview suspects");
+        poirotsToDos.add("let the little grey cells do their work");
+        poirotsToDos.add("trim mustache");
+        poirotsToDos.add("call all suspects together");
+        poirotsToDos.add("reveal the truth of the crime");
+
+        System.out.println(sherlocksToDos.size());
+        System.out.println(poirotsToDos.size());
+
+        System.out.println("Poirot");
     }
 }
