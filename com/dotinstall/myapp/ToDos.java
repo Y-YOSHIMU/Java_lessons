214ca9def3c16583267024504c2d2860924dba75
Y-YOSHIMU <mcu7.oldtr.uc@gmail.com>
2021-01-30 18:16:54 +0900

    [Add]Review

--- com/dotinstall/myapp/ToDos.java
+++ com/dotinstall/myapp/ToDos.java
@@ -1,5 +1,13 @@
 import java.util.ArrayList;
 
+// Creating an ArrayList.
+// Adding a new ArrayList item using add().
+// Accessing the size of an ArrayList using size().
+// Finding an item by index using get().
+// Changing the value of an ArrayList item using set().
+// Removing an item with a specific value using remove().
+// Retrieving the index of an item with a specific value using indexOf().
+
 class ToDos {
     
     public static void main(String[] args) {
