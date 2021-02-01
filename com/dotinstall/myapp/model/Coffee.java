d7aba854dd6afe1b1b9a7de583d950f5eb9f0f12
Y-YOSHIMU <mcu7.oldtr.uc@gmail.com>
2021-02-02 01:08:09 +0900

    [Add]Using For Loops

--- com/dotinstall/myapp/model/Coffee.java
+++ com/dotinstall/myapp/model/Coffee.java
@@ -1,11 +1,15 @@
 class Coffee {
     
     Public static void main(String[] args) {
-        int cupsOfCoffee = 1;
+        // int cupsOfCoffee = 1;
 
-        while (cupsOfCoffee <= 100) {
-            System.out.println("Fry drinks cup of coffee #" + cupsOfCoffee);
-            cupsOfCoffee++;
+        // while (cupsOfCoffee <= 100) {
+        //     System.out.println("Fry drinks cup of coffee #" + cupsOfCoffee);
+        //     cupsOfCoffee++;
+        // }
+
+        for (int i = 1; i <= 100; i++) {
+            System.out.println("Fry drinks cup of coffee #" + i);
         }
     }
 }
