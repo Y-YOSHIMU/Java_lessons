576dbc0ef716e5c364ed6cebeb26e8bb8fd81475
Y-YOSHIMU <mcu7.oldtr.uc@gmail.com>
2021-02-08 01:39:22 +0900

    [Add]concat()

---
+++ com/dotinstall/myapp/model/CombineNames.java
@@ -0,0 +1,10 @@
+public class CombineNames {
+
+    public static void main(String[] args) {
+
+        String firstName = "Yoshimura";
+        String lastName = "Yuya";
+
+        System.out.println(firstName.concat(lastName));
+    }
+}
