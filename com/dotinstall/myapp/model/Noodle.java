42829b745124bdff1819cd74271d508f684a6408
Y-YOSHIMU <mcu7.oldtr.uc@gmail.com>
2021-03-03 06:01:36 +0900

    [Add]Child Classes in Arrays and ArrayLists

--- com/dotinstall/myapp/model/Noodle.java
+++ com/dotinstall/myapp/model/Noodle.java
@@ -24,9 +24,24 @@ class Noodle {
         return true;
     }
 
+    public String getCookPrep() {
+        return "Boil noodle for 7 minutes and add sauce.";
+    }
+
     public static void main(String[] args) {
-        Spaetzle kaesespaetzle = new Spaetzle();
-        kaesespaetzle.cook();
+        Noodle spaghetti, ramen, pho;
+
+        spaghetti = new Spaghetti();
+        ramen = new Ramen();
+        pho = new Pho();
+
+        Noodle[] allTheNoodles = {spaghetti, ramen, pho};
+        for (Noodle noodle : allTheNoodles) {
+            System.out.println(noodle.getCookPrep());
+        }
+
+        // Spaetzle kaesespaetzle = new Spaetzle();
+        // kaesespaetzle.cook();
 
         // Ramen yasaiRamen = new Ramen();
         // System.out.println(yasaiRamen.ingredients);
